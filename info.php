a87d28657d06578bbbadcc10e8b63f698642fba0
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2022-01-25 14:26:44 +0100

    Update 1.3.2
    - Info pagina aangemaakt

---
+++ info.php
@@ -0,0 +1,34 @@
+<?php
+include "functions.php";
+?>
+
+<html>
+    <head>
+        <title>Scheduler</title>
+        <!-- Bootstrap core CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    <body>
+        <main class="container">
+            <div class="bg-light p-5 rounded mt-3">
+                <a class="btn btn-primary" href="/">Ga terug</a>
+                <h1 class="text-center">Scheduler info</h1>
+                <br>
+                <h3>Hoe maak ik een nieuw account aan?</h3>
+                <p>Stap 1: Log in op de <u>site</u> van ItsLearning en ga naar de agenda</p>
+                <img class="img-fluid" src="dist/img/step1.png" \>
+                <p>Stap 2: Klik op de 3 puntjes aan de rechterkant van het scherm en klik vervolgens op "Abonneren"</p>
+                <img class="img-fluid" src="dist/img/step2.png" \>
+                <p>Stap 3: Kopieer de link die op het scherm verschijnt</p>
+                <img class="img-fluid" src="dist/img/step3.png" \>
+                <p><span style="color: red;">LET OP:</span> niet de volledige link is zichtbaar. Kopieer daarom de link door er met de rechtermuisknop op te klikken en te klikken op "Kopieer link adres"</p>
+                <img class="img-fluid" src="dist/img/copy.png" \>
+                <p>Stap 4: Ga naar <a href="https://scheduler.rickokkersen.ga">https://scheduler.rickokkersen.ga</a> en plak de link in het veld bij URL.</p>
+                <img class="img-fluid" src="dist/img/step4.png" \>
+                <p>Stap 5: Vul je naam in en klik op "Maak profiel"</p><br>
+                <img class="img-fluid" src="dist/img/step5.png" \>
+                <p>Als je deze stappen zorgvuldig gevolgd hebt zie je nu je persoonlijke code op het scherm staan. Bewaar deze code goed! Ieder half uur worden de roosters van alle aangemelde leerlingen vernieuwd. Kijk dus pas na het hele of halve uur op je persoonlijke roosterpagina. Kom je er dan nog steeds niet uit? Stuur me dan een mailtje op <a href="mailto:st126151@leerling.stellamariscollege.nl">st126151@leerling.stellamariscollege.nl</a></p>
+            </div>
+        </main>
+    </body>
+</html>
