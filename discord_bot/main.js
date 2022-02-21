9020542d74d794070a18f6f301c52439bbd80edc
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2022-02-21 20:30:13 +0100

    Update main.js

--- discord_bot/main.js
+++ discord_bot/main.js
@@ -1,9 +1,12 @@
-const Discord = require("discord.js");
+const fs = require('fs');
+// require('log-timestamp');
+
+const {Client, MessageEmbed} = require("discord.js");
 const config = require("./config.json");
 
 const prefix = "!";
 
-const bot = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+const bot = new Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 
 bot.login(config.BOT_TOKEN);
 console.log("Bot started");
@@ -15,7 +18,39 @@ bot.on("messageCreate", function(message) {
     const commandBody = message.content.slice(prefix.length);
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
-    const notify_channel = config['channels'][message.guildId.toString()]['notify'];
-
     
+
+});
+
+fs.watchFile('../events/updates.json', () => {
+    console.log('File changed');
+
+    const notify_channel_id = config['channels']['851114680994103336']['notify'];
+    const notify_channel = bot.channels.cache.get(notify_channel_id);
+
+    var currentdate = new Date();
+    var datetime = "Laatste update: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    notify_channel.setTopic(datetime)
+    .then(updated => console.log(`Channel's new topic is ${updated.topic}`))
+    .catch(console.error);
+
+    var updates = require("../events/updates.json");
+
+    updates.forEach(update => {
+        const embed = new MessageEmbed()
+            .setColor(update.color)
+            .setTitle(update.title)
+            .setDescription(update.description);
+        update.fields.forEach(field => {
+            embed.addField(field.name, field.value, true);
+        });
+        const ping = config.roles[update.vak];
+        notify_channel.send({content: `<@&${ping}>`, embeds: [embed]});
+        
+    });
 });
